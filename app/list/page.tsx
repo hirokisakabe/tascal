96056a6ee573084492e2ba722f1775644f5bd0d7
Hiroki SAKABE <hiroki.sakabe@icloud.com>
2024-04-14 11:31:08 +0900

    タスクを完了できるようにする

--- app/list/page.tsx
+++ app/list/page.tsx
@@ -1,16 +1,20 @@
-import { Checkbox } from "@nextui-org/react";
 import AddTaskButton from "./_components/AddTaskButton";
+import ShowCompleteTaskCheckbox from "./_components/ShowCompleteTaskCheckbox";
 import { TaskCard } from "./_components/TaskCard";
 import { fetchTasks } from "./_lib/fetchTask";
 
-export default async function Page() {
-	const tasks = await fetchTasks();
+export default async function Page({
+	searchParams,
+}: { searchParams: { [key: string]: string | string[] | undefined } }) {
+	const showCompleteTask = searchParams?.showCompleteTask === "true";
+
+	const tasks = await fetchTasks({ includeCompleted: showCompleteTask });
 
 	return (
 		<div>
 			<div className="flex justify-between py-3">
 				<AddTaskButton />
-				<Checkbox>完了済のタスクも表示する</Checkbox>
+				<ShowCompleteTaskCheckbox />
 			</div>
 			<div className="grid gap-0.5">
 				{tasks.map((task) => (
