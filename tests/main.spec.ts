6ade9dd33a5770fd1f1ced20b61aab8a069b7024
Hiroki SAKABE <hiroki.sakabe@icloud.com>
2024-06-23 02:01:15 +0900

    テストコードを記述

---
+++ tests/main.spec.ts
@@ -0,0 +1,7 @@
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('http://localhost:3000/list');
+
+  await expect(page).toHaveTitle(/tascal/);
+});
